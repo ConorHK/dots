1d8e3cc2058dd51ccbd7cf7f7966dce561b14415
ConorHK <conorknowles@gmail.com>
2022-03-03 19:11:01 +0000

    Remove extra space

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -57,4 +57,4 @@ git_prompt() {
   esac
   echo " on %B"${color_git}${ref} ${stat}"%b"
 }
-PROMPT='%F{15}%B%(5~|%-1|%3~|%4~)%b$(git_prompt) ${color_prompt}──── ─${color_normal} '
+PROMPT='%F{15}%B%(5~|%-1|%3~|%4~)%b$(git_prompt)${color_prompt}──── ─${color_normal} '
