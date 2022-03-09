1638f44f7d2f4dc8422bad96a2b20f3f97e036f3
ConorHK <dev@conorknowles.com>
2022-03-09 19:45:55 +0000

    Refactor aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,34 +1,26 @@
-if  type "exa" > /dev/null; then
+if which "exa" > /dev/null; then
   alias ls="exa --icons"
   alias l="exa --icons -la"
 fi
-if  type "zathura" > /dev/null; then
-  alias pdf="zathura"
-fi
-if  type "pacman" > /dev/null; then
-  alias p="sudo pacman"
-fi
-if  type "ranger" > /dev/null; then
-  alias ra="ranger"
-fi
-if  type "nvim" > /dev/null; then
-  alias vim="nvim"
-fi
-if type "dragon-drag-and-drop" > /dev/null; then
-  alias dragon="dragon-drag-and-drop"
-fi
-if type "spotifyd" > /dev/null; then
-  alias spt="spotifyd && spt"
+
+if which "git" > /dev/null; then
+  alias gs="git status"
+  alias gc="git commit"
+  alias ga="git add"
+  alias gaa="git add --all"
+  alias gd="git diff"
+  alias gp="git push"
+  alias gr="git rebase"
+  alias gm="git merge"
 fi
 
-alias gt="git log --graph --decorate --pretty=oneline --abbrev-commit"
-alias gs="git status"
-alias gc="git commit"
-alias ga="git add"
-alias gaa="git add --all"
-alias gd="git diff"
-alias gp="git push"
+[ which "zathura" &> /dev/null ] && alias pdf="zathura"
+[ which "pacman" &> /dev/null ] && alias p="sudo pacman"
+[ which "ranger" &> /dev/null ] && alias ra="ranger"
+[ which "nvim" &> /dev/null ] && alias vim="nvim"
+[ which "dragon-drag-and-drop" &> /dev/null ] && alias dragon="dragon-drag-and-drop"
+[ which "spotifyd" &> /dev/null ] && alias spt="spotifyd && spt"
+[ which "mpv" &> /dev/null ] &&  [ "$HOSTNAME = "$LAPTOP ] && alias selfie="mpv av://v4l2:/dev/video0 --profile=low-latency --untimed"
 
 alias dr="dirs -v"
 alias source-zsh="source $XDG_CONFIG_HOME/zsh/.zshrc"
-[[ "$HOSTNAME" == "$LAPTOP" ]] && alias selfie="mpv av://v4l2:/dev/video0 --profile=low-latency --untimed"
