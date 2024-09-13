b5b8194b56416a16d643b4f932bfe500989121c7
Conor Knowles <knoconor@amazon.com>
2024-09-13 11:04:51 +0000

    Barebones nix setup

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "HomeManager dotfiles flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs: {
+    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+    defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
+ 
+    homeConfigurations = {
+      "conor" = inputs.home-manager.lib.homeManagerConfiguration {
+        system = "x86_64-linux";
+        homeDirectory = "/home/knoconor";
+        username = "conor"; # TODO: Change to your username
+        configuration.imports = [ ./home.nix ];
+      };
+      "knoconor" = inputs.home-manager.lib.homeManagerConfiguration {
+        system = "x86_64-linux";
+        homeDirectory = "/home/knoconor";
+        username = "knoconor";
+        configuration.imports = [ ./home.nix ];
+      };
+    };
+  };
+}
